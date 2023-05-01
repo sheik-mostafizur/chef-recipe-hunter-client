17e8549375e0848cab30acb464af15fdae967ac4
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-05-01 22:26:36 +0600

    add blog page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "../pages/Home";
 import ErrorPage from "../pages/ErrorPage";
+import Blog from "../pages/Blog";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
   },
 ]);
 export default router;
