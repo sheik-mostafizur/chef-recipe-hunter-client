81429b65579fa18e316989df400245df0ea3d8a8
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-05-02 10:12:29 +0600

    add Footer section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Banner from "./Banner";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Footer />
     </div>
   );
 };
